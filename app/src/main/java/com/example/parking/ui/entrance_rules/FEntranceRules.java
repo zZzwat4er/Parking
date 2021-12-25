90e42bf9b28f8b37e49d213ae1402367f7adccd2
zZzwat4er <valenten2001@mail.ru>
2021-12-25 21:27:57 +0300

    rules text

--- app/src/main/java/com/example/parking/ui/entrance_rules/FEntranceRules.java
+++ app/src/main/java/com/example/parking/ui/entrance_rules/FEntranceRules.java
@@ -1,9 +1,11 @@
 package com.example.parking.ui.entrance_rules;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,7 +17,9 @@ public class FEntranceRules extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.f_entrance_rules, container, false);
-
+        String text = getString(R.string.entrance_rules_text);
+        TextView textView = root.findViewById(R.id.entrance_rules_text);
+        textView.setText(Html.fromHtml(text), TextView.BufferType.SPANNABLE);
         return root;
     }
 
--- app/src/main/java/com/example/parking/ui/user_rules/FUserRules.java
+++ app/src/main/java/com/example/parking/ui/user_rules/FUserRules.java
@@ -1,9 +1,12 @@
 package com.example.parking.ui.user_rules;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,7 +18,9 @@ public class FUserRules extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.f_user_rules, container, false);
-
+        String userRuleText = getString(R.string.user_rules_text);
+        TextView textView = root.findViewById(R.id.user_rules_text);
+        textView.setText(Html.fromHtml(userRuleText), TextView.BufferType.SPANNABLE);
         return root;
     }
 
